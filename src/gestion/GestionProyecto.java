d0129d49aa828d8e9422ab0d4b9b13d682a496a4
Emilia Ocampo <emilia4630709@gmail.com>
2024-11-14 17:31:03 -0300

    Agregar proyectos al archivo

--- src/gestion/GestionProyecto.java
+++ src/gestion/GestionProyecto.java
@@ -1,11 +1,184 @@
 package gestion;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import proyecto.Proyecto;
+import proyecto.Tarea;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class GestionProyecto {
 
+    private String nomJSON = "proyectos.json";
+    private ArrayList<Proyecto> proyectos;
+    public GestionProyecto() {
+        proyectos = new ArrayList<>();
+    }
 
+    public void removeProyecto(Proyecto proyecto){
+        proyectos.remove(proyecto);
+    }
 
+    public void addProyecto(Proyecto proyecto){
+       proyectos.add(proyecto);
+    }
 
+    public JSONObject serializarListaProyectos(ArrayList<Proyecto> proyectos) {
+        JSONObject proyectosJSON = null;
+        JSONArray listaProyectos = null;
+        try{
+            proyectosJSON = new JSONObject();
+            listaProyectos = new JSONArray();
+            for (Proyecto proyecto : proyectos) {
+                listaProyectos.put(proyecto.serializar());
+            }
 
+            proyectosJSON.put("proyectos", listaProyectos);
 
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return proyectosJSON;
+    }
+
+    public ArrayList<Proyecto> deserializarListaProyectos(JSONObject proyectosJSON) {
+        ArrayList<Proyecto> proyectos = new ArrayList<>();
+
+        try {
+            // Obtener el JSONArray que contiene los proyectos
+            JSONArray listaProyectos = proyectosJSON.getJSONArray("proyectos");
+
+            for (int i = 0; i < listaProyectos.length(); i++) {
+                // Obtener cada JSONObject del JSONArray
+                JSONObject proyectoJSON = listaProyectos.getJSONObject(i);
+
+                // Usar tu método existente que deserializa un solo proyecto
+                Proyecto proyecto = new Proyecto(proyectoJSON);
+
+                // Añadir el proyecto deserializado a la lista
+                proyectos.add(proyecto);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return proyectos;
+    }
+
+    public void guardarProyectosEnArchivo(JSONObject proyectosJSON) {
+        OperacionesLectoEscritura.grabar(nomJSON, proyectosJSON);
+    }
+
+    public JSONObject leerArchivoProyectos(String nombreArchivo) {
+        StringBuilder contenido = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+
+            // Leer línea por línea
+            while ((linea = bufferedReader.readLine()) != null) {
+                contenido.append(linea);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Convertir el contenido al JSONObject
+        return new JSONObject(contenido.toString());
+    }
+
+    public void agregarTareaAlProyecto(int idProyecto, Tarea nuevaTarea) {
+        JSONObject proyectosJSON = null;
+        JSONArray proyectosArray = null;
+        try {
+            proyectosJSON = leerArchivoProyectos(nomJSON);
+            proyectosArray = proyectosJSON.getJSONArray("proyectos");
+            boolean proyectoEncontrado = false;
+
+            for (int i = 0; i < proyectosArray.length(); i++) {
+                JSONObject proyectoJSON = proyectosArray.getJSONObject(i);
+
+                // Suponiendo que cada proyecto tiene un ID
+                if (proyectoJSON.getInt("id") == idProyecto) {
+                    proyectoEncontrado = true;
+
+                    // Crear el JSONObject de la nueva tarea
+                    JSONObject tareaJSON = nuevaTarea.serializar();
+
+                    // Agregar la tarea al proyecto
+                    JSONArray tareasArray = proyectoJSON.getJSONArray("tareas");
+                    tareasArray.put(tareaJSON);
+
+                    break; // Salir del bucle una vez que encontramos el proyecto
+                }
+            }
+
+            if (proyectoEncontrado) {
+                // Serializar el objeto `proyectosJSON` nuevamente al archivo
+                guardarProyectosEnArchivo(proyectosJSON);
+            } else {
+                System.out.println("Proyecto no encontrado con ID: " + idProyecto);
+            }
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void eliminarTareaDelProyecto(int idProyecto, int idTarea) {
+        JSONObject proyectosJSON = null;
+        JSONArray proyectosArray = null;
+
+        try {
+            proyectosJSON = leerArchivoProyectos(nomJSON);
+            proyectosArray = proyectosJSON.getJSONArray("proyectos");
+            boolean proyectoEncontrado = false;
+
+            for (int i = 0; i < proyectosArray.length(); i++) {
+                JSONObject proyectoJSON = proyectosArray.getJSONObject(i);
+
+                // Verificar si el proyecto coincide con el ID proporcionado
+                if (proyectoJSON.getInt("id") == idProyecto) {
+                    proyectoEncontrado = true;
+
+                    // Obtener el arreglo de tareas del proyecto
+                    JSONArray tareasArray = proyectoJSON.getJSONArray("tareas");
+                    boolean tareaEncontrada = false;
+
+                    // Buscar la tarea por su ID y eliminarla si se encuentra
+                    for (int j = 0; j < tareasArray.length(); j++) {
+                        JSONObject tareaJSON = tareasArray.getJSONObject(j);
+
+                        if (tareaJSON.getInt("id") == idTarea) {
+                            tareasArray.remove(j); // Eliminar la tarea
+                            tareaEncontrada = true;
+                            break; // Salir del bucle una vez que encontramos la tarea
+                        }
+                    }
+
+                    if (!tareaEncontrada) {
+                        System.out.println("Tarea no encontrada con ID: " + idTarea);
+                    }
+
+                    break; // Salir del bucle una vez que encontramos el proyecto
+                }
+            }
+
+            if (proyectoEncontrado) {
+                // Serializar el objeto `proyectosJSON` nuevamente al archivo
+                guardarProyectosEnArchivo(proyectosJSON);
+            } else {
+                System.out.println("Proyecto no encontrado con ID: " + idProyecto);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
