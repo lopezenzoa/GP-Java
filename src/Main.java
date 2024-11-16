65b952f4519f576be2de2288a11f9e32ab85b38a
Enzo Lopez <enzoagustinlopez2003@gmail.com>
2024-11-16 15:51:13 -0300

    Carga archivos .json con inrformacion ficticia

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import enums.Rol;
+import exception.UsuarioExisteException;
+import exception.UsuarioNoEncontradoException;
 import gestion.GestionProyecto;
 import gestion.GestionUsuarios;
 import gestion.OperacionesLectoEscritura;
@@ -22,141 +24,118 @@ public class Main  {
 
         Administrador Monica = new Administrador("Monica", "Richiardi", "moni@hotmail.com","Computer Science");
         Administrador Cesar = new Administrador("Cesar", "II", "ceseremperador@hotmail.com","Emperador");
+        Administrador Lucas = new Administrador("Lucas", "González", "lucas.gonzalez@example.com", "Information Systems");
+        Administrador Carla = new Administrador("Carla", "Ramírez", "carla.ramirez@example.com", "Software Engineering");
+        Administrador Diego = new Administrador("Diego", "Fernández", "diego.fernandez@example.com", "Data Science");
 
         Lider Mike = new Lider("Mike","Wazowszki","Mikewazawski@gmail.com","Asustador");
         Lider Sullivan = new Lider("Sullivan","Mikelson","sullivan@gmail.com","Asustador");
+        Lider Randall = new Lider("Randall", "Boggs", "randall.boggs@gmail.com", "Asustador Experto");
+        Lider Roz = new Lider("Roz", "Doe", "roz.doe@gmail.com", "Supervisora");
+        Lider Celia = new Lider("Celia", "Mae", "celia.mae@gmail.com", "Recepcionista");
 
         Tarea tarea1 = new Tarea("Sprint 1","Creamos e instanciamos las clases. Primer commit", Enzo);
         Tarea tarea2 = new Tarea("Sprint 2","Instanciar clases. Segundo commit", Lionel);
         Tarea tarea3 = new Tarea("Sprint 3","Primer prueba", Angel);
+        Tarea tarea4 = new Tarea("Sprint 2", "Implementamos los métodos principales y realizamos pruebas iniciales", Sergio);
+        Tarea tarea5 = new Tarea("Sprint 3", "Optimizamos el código y documentamos el proyecto", Emiliano);
 
-        Proyecto nuevoProyecto = new Proyecto(Monica, Mike,"TPFINAL");
+        Proyecto p1 = new Proyecto(Monica, Mike,"TPFINAL");
+        Proyecto p2 = new Proyecto(Cesar, Sullivan, "Aplicación Web");
+        Proyecto p3 = new Proyecto(Lucas, Randall, "Diseño de la DB");
+        Proyecto p4 = new Proyecto(Carla, Roz, "Rediseño de la pagina principal");
+        Proyecto p5 = new Proyecto(Diego, Celia, "Otro Proyecto");
 
-        nuevoProyecto.agregarMiembro(Enzo);
-        nuevoProyecto.agregarMiembro(Lionel);
-        nuevoProyecto.agregarMiembro(Sergio);
-        nuevoProyecto.agregarMiembro(Angel);
-        nuevoProyecto.agregarMiembro(Emiliano);
+        GestionProyecto gestionProyecto = new GestionProyecto();
 
-        nuevoProyecto.agregarTarea(tarea1);
-        nuevoProyecto.agregarTarea(tarea2);
-        nuevoProyecto.agregarTarea(tarea3);
+        gestionProyecto.addProyecto(p1);
+        gestionProyecto.addProyecto(p2);
+        gestionProyecto.addProyecto(p3);
+        gestionProyecto.addProyecto(p4);
+        gestionProyecto.addProyecto(p5);
 
-        nuevoProyecto.eliminarMiembro(Lionel); // Funciona
-        nuevoProyecto.eliminarTareaPorId(tarea2.getId()); //Funciona
-        // System.out.println(nuevoProyecto.existeTarea(tarea2)); //Funciona
-        // System.out.println(nuevoProyecto.getEquipo()); // Hacer metodo en clase gestora que busque por ID los usuarios
-
-        // System.out.println(nuevoProyecto.toString());
-
-        /* Prueba de serializacion de clases referidas al usuario */
-        JSONObject enzoJSON = Enzo.serializar();
-        JSONObject sullivanJSON = Sullivan.serializar();
-        JSONObject monicaJSON = Monica.serializar();
-
-        /* Prueba de deserializacion de clases referidas al usuario */
-        MiembroEquipo Enzo_2 = new MiembroEquipo(enzoJSON);
-        Lider sullivan_2 = new Lider(sullivanJSON);
-        Administrador monica_2 = new Administrador(monicaJSON);
-
-        // MiembroEquipo
+        /* Agregando los JSONObject para el archivo (15 usuarios) */
         /*
-        System.out.println(enzoJSON);
-        System.out.println(Enzo_2);
-        System.out.println();
+        try {
+            GestionUsuarios.agregarUsuario(Monica);
+            GestionUsuarios.agregarUsuario(Cesar);
+            GestionUsuarios.agregarUsuario(Lucas);
+            GestionUsuarios.agregarUsuario(Carla);
+            GestionUsuarios.agregarUsuario(Diego);
 
-        // Lider
-        System.out.println(sullivanJSON);
-        System.out.println(sullivan_2);
-        System.out.println();
+            GestionUsuarios.agregarUsuario(Mike);
+            GestionUsuarios.agregarUsuario(Sullivan);
+            GestionUsuarios.agregarUsuario(Randall);
+            GestionUsuarios.agregarUsuario(Roz);
+            GestionUsuarios.agregarUsuario(Celia);
 
-        // Administrador
-        System.out.println(monicaJSON);
-        System.out.println(monica_2);
+            GestionUsuarios.agregarUsuario(Enzo);
+            GestionUsuarios.agregarUsuario(Lionel);
+            GestionUsuarios.agregarUsuario(Sergio);
+            GestionUsuarios.agregarUsuario(Angel);
+            GestionUsuarios.agregarUsuario(Emiliano);
+        } catch (UsuarioExisteException e) {
+            System.err.println(e.getMessage());
+        }
 
          */
 
         /* Prueba de serializacion de Proyecto y Tarea */
-        JSONObject p1JSON = nuevoProyecto.serializar();
-
+        // Generando los objetos con informacion ficticia
         /*
-        System.out.println();
-        System.out.println(p1JSON);
-        System.out.println();
+        JSONObject proyectosJSON = gestionProyecto.serializarListaProyectos();
+        gestionProyecto.guardarProyectosEnArchivo(proyectosJSON);
+
+        gestionProyecto.agregarTareaAlProyecto(p1.getId(), tarea1);
+        gestionProyecto.agregarTareaAlProyecto(p1.getId(), tarea2);
+        gestionProyecto.agregarTareaAlProyecto(p1.getId(), tarea3);
+        gestionProyecto.agregarTareaAlProyecto(p1.getId(), tarea4);
+        gestionProyecto.agregarTareaAlProyecto(p1.getId(), tarea5);
+
+        gestionProyecto.agregarTareaAlProyecto(p2.getId(), tarea1);
+        gestionProyecto.agregarTareaAlProyecto(p2.getId(), tarea2);
+        gestionProyecto.agregarTareaAlProyecto(p2.getId(), tarea3);
+        gestionProyecto.agregarTareaAlProyecto(p2.getId(), tarea4);
+
+        gestionProyecto.agregarTareaAlProyecto(p3.getId(), tarea1);
+        gestionProyecto.agregarTareaAlProyecto(p3.getId(), tarea2);
+        gestionProyecto.agregarTareaAlProyecto(p3.getId(), tarea3);
+
+        gestionProyecto.agregarTareaAlProyecto(p4.getId(), tarea1);
+        gestionProyecto.agregarTareaAlProyecto(p4.getId(), tarea2);
+
+        gestionProyecto.agregarTareaAlProyecto(p5.getId(), tarea1);
+
+        try {
+            gestionProyecto.agregarMiembroAlEquipo(p1.getId(), Enzo);
+            gestionProyecto.agregarMiembroAlEquipo(p1.getId(), Lionel);
+            gestionProyecto.agregarMiembroAlEquipo(p1.getId(), Sergio);
+            gestionProyecto.agregarMiembroAlEquipo(p1.getId(), Angel);
+            gestionProyecto.agregarMiembroAlEquipo(p1.getId(), Emiliano);
+
+            gestionProyecto.agregarMiembroAlEquipo(p2.getId(), Enzo);
+            gestionProyecto.agregarMiembroAlEquipo(p2.getId(), Lionel);
+            gestionProyecto.agregarMiembroAlEquipo(p2.getId(), Sergio);
+            gestionProyecto.agregarMiembroAlEquipo(p2.getId(), Angel);
+
+            gestionProyecto.agregarMiembroAlEquipo(p3.getId(), Enzo);
+            gestionProyecto.agregarMiembroAlEquipo(p3.getId(), Lionel);
+            gestionProyecto.agregarMiembroAlEquipo(p3.getId(), Sergio);
+
+            gestionProyecto.agregarMiembroAlEquipo(p4.getId(), Enzo);
+            gestionProyecto.agregarMiembroAlEquipo(p4.getId(), Lionel);
+
+            gestionProyecto.agregarMiembroAlEquipo(p5.getId(), Enzo);
+        } catch (UsuarioExisteException e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            gestionProyecto.eliminarMiembroDelEquipo(p1.getId(), Enzo.getId());
+        } catch (UsuarioNoEncontradoException e) {
+            System.err.println(e.getMessage());
+        }
 
          */
-
-        Proyecto nuevoProyecto_2 = new Proyecto(p1JSON);
-
-        // System.out.println(nuevoProyecto_2);
-
-        /* Pruebas de la clase GestionUsuarios */
-
-
-        /*
-        GestionUsuarios.agregarUsuario(Enzo);
-        GestionUsuarios.agregarUsuario(Mike);
-        GestionUsuarios.agregarUsuario(Monica);
-
-         */
-
-        /*
-        GestionUsuarios.eliminarUsuario(Enzo);
-        GestionUsuarios.eliminarUsuario(Mike);
-        GestionUsuarios.eliminarUsuario(Monica);
-
-         */
-
-        // GestionUsuarios.agregarUsuario(Lionel);
-
-        GestionUsuarios.mostrarUsuariosActivos();
-        System.out.println();
-        // GestionUsuarios.mostrarUsuariosInactivos();
-
-        // Probando los metodos para comprobar la existencia dentro del archivo
-        /*
-        System.out.println(GestionUsuarios.comprobarExistencia(Enzo));
-        System.out.println(GestionUsuarios.comprobarExistencia(Sullivan));
-
-         */
-
-        // Probando los metodos para encontrar un usuario
-        /*
-        System.out.println(GestionUsuarios.buscarMiembroEquipo(82));
-        System.out.println(GestionUsuarios.buscarLider(99));
-        System.out.println(GestionUsuarios.buscarAdministrador(77));
-        System.out.println(GestionUsuarios.buscarAdministrador(45));
-
-<<<<<<< HEAD
-        GestionProyecto proyectos = new GestionProyecto();
-
-        proyectos.addProyecto(nuevoProyecto);
-        proyectos.guardarProyectosEnArchivo(proyectos.serializarListaProyectos());
-        ArrayList<Proyecto> aaa = new ArrayList<>();
-        aaa = proyectos.deserializarListaProyectos(proyectos.leerArchivoProyectos());
-
-        System.out.println("leido correctamente " + aaa.toString());
-
-
-
-=======
-         */
-
-        // Probando los metodos para modificar usuarios
-        /*
-        MiembroEquipo usuarioViejo = GestionUsuarios.buscarMiembroEquipo(59);
-        Lider liderViejo = GestionUsuarios.buscarLider(99);
-        Administrador administradorViejo = GestionUsuarios.buscarAdministrador(77);
-
-        System.out.println(GestionUsuarios.modificarUsuario(usuarioViejo, Enzo));
-        System.out.println(GestionUsuarios.modificarUsuario(liderViejo, Sullivan));
-        System.out.println(GestionUsuarios.modificarUsuario(administradorViejo, Cesar));
-
-        System.out.println();
-
-        GestionUsuarios.mostrarUsuariosActivos();
-
-         */
->>>>>>> 24c03926c0266f81b66eed42ed1e1ba9162a7d3e
     }
 }
